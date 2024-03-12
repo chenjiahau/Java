4bc09245f550d4043854b6bc0276bbad48875854
chenjiahau <chenjiahau@gmail.com>
2024-03-12 15:14:37 +0800

    Remove packages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,5 @@
-package org;
-
-import org.animal.Animal;
-
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        System.out.println(animal);
+        System.out.println("Hello, World!");
     }
 }
