cb813e4c074ca685412714ab9fb0818afa11de66
chenjiahau <chenjiahau@gmail.com>
2024-03-12 15:14:16 +0800

    Remove all packages.

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package org;
+
+import org.animal.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        System.out.println(animal);
+    }
+}
